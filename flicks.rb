07c1ea1fd40175dad2eef1c8344f395bf148f942
David G Chaves <davidgchaves@gmail.com>
2014-09-02 14:39:35 +0200

    07 - classes

--- flicks.rb
+++ flicks.rb
@@ -1,13 +1,29 @@
-def weekday
-  Time.new.strftime "%A"
+class Movie
+  def initialize(title, rank=0)
+    @title = title.capitalize
+    @rank = rank
+  end
+
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+
+  def to_s
+    "#{@title} has a rank of #{@rank}"
+  end
 end
 
-def movie_listing(title, rank=0)
-  "#{weekday.upcase}: #{title.capitalize} has a rank of #{rank}."
-end
+movie1 = Movie.new "goonies", 10
+movie1.thumbs_up
+puts movie1
 
-puts movie_listing "goonies", 10
-puts movie_listing "ghostbusters", 9
+movie2 = Movie.new "ghostbusters", 9
+movie2.thumbs_down
+puts movie2
 
-a_title = "goldfinger"
-puts movie_listing a_title
+movie3 = Movie.new "goldfinger"
+puts movie3
