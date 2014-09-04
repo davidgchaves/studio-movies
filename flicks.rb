c3ef2e25bd957570c01eb30c908d90359f4eb892
David G Chaves <davidgchaves@gmail.com>
2014-09-04 21:01:40 +0200

    08 - attributes

--- flicks.rb
+++ flicks.rb
@@ -1,7 +1,14 @@
 class Movie
+  attr_accessor :title
+  attr_reader :rank
+
   def initialize(title, rank=0)
     @title = title.capitalize
     @rank = rank
+  end
+
+  def normalized_rank
+    @rank / 10
   end
 
   def thumbs_up
@@ -13,17 +20,14 @@ class Movie
   end
 
   def to_s
-    "#{@title} has a rank of #{@rank}"
+    "#{@title} has a normalized rank of #{normalized_rank}"
   end
 end
 
-movie1 = Movie.new "goonies", 10
+movie1 = Movie.new "goonies", 100
 movie1.thumbs_up
 puts movie1
-
-movie2 = Movie.new "ghostbusters", 9
-movie2.thumbs_down
-puts movie2
-
-movie3 = Movie.new "goldfinger"
-puts movie3
+movie1.title = "Goonies 2.0"
+puts movie1.title
+puts movie1.rank
+puts movie1.normalized_rank
