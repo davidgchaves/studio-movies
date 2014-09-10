77000b1217f5a3667aceb75f499cd1b65f3408bf
David G Chaves <davidgchaves@gmail.com>
2014-09-10 13:59:45 +0200

    13 - conditionals and tdd part 1

--- movie_spec.rb
+++ movie_spec.rb
@@ -14,7 +14,7 @@ describe Movie do
   end
 
   it "has a string representation" do
-    expect(@movie.to_s).to eq "Goonies has a rank of 10"
+    expect(@movie.to_s).to eq "Goonies has a rank of 10 (Hit)"
   end
 
   context "when created without a rank" do
@@ -38,6 +38,28 @@ describe Movie do
 
     it "decreases rank by 1" do
       expect(@movie.rank).to eq initial_rank - 1
+    end
+  end
+
+  context "with 10 or more rank" do
+    it "is a hit" do
+      expect(@movie.hit?).to be true
+    end
+
+    it "has a hit status" do
+      expect(@movie.status).to eq "Hit"
+    end
+  end
+
+  context "with less than 10 rank" do
+    before { @movie = Movie.new "goonies", 9 }
+
+    it "is not a hit" do
+      expect(@movie.hit?).to be false
+    end
+
+    it "has a flop status" do
+      expect(@movie.status).to eq "Flop"
     end
   end
 end
