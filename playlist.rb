a3ae5bb8cbe61168864042750d5ed7271983c63d
David G Chaves <davidgchaves@gmail.com>
2014-09-10 19:30:40 +0200

    13 - conditionals and tdd part 2

--- playlist.rb
+++ playlist.rb
@@ -10,12 +10,25 @@ class Playlist
     @movies.push movie
   end
 
+  def roll_die
+    rand 1..6
+  end
+
   def play
     puts "#{@name}'s playlist:"
     puts @movies
 
     @movies.each do |movie|
-      movie.thumbs_up
+      case roll_die
+      when 1..2
+        movie.thumbs_down
+        puts "#{movie.title} got a thumbs down."
+      when 3..4
+        puts "#{movie.title} was skipped."
+      when 5..6
+        movie.thumbs_up
+        puts "#{movie.title} got a thumbs up!"
+      end
       puts movie
     end
   end
---
+++ playlist_spec.rb
@@ -0,0 +1,40 @@
+require_relative 'playlist'
+
+describe Playlist do
+  before do
+    $stdout = StringIO.new      #supress (redirect) the standard output
+    @playlist = Playlist.new "Kermit"
+  end
+
+  context "being played with one movie" do
+    before do
+      @initial_rank = 10
+      @movie = Movie.new "Goonies", @initial_rank
+      @playlist.add_movie @movie
+    end
+
+    it "thumbs up the movie if a high number is rolled" do
+      allow(@playlist).to receive(:roll_die) { 5 }
+
+      @playlist.play
+
+      expect(@movie.rank).to eq @initial_rank + 1
+    end
+
+    it "skips the movie if a medium number is rolled" do
+      allow(@playlist).to receive(:roll_die) { 3 }
+
+      @playlist.play
+
+      expect(@movie.rank).to eq @initial_rank
+    end
+
+    it "thumbs down the movie if a low number is rolled" do
+      allow(@playlist).to receive(:roll_die) { 1 }
+
+      @playlist.play
+
+      expect(@movie.rank).to eq @initial_rank - 1
+    end
+  end
+end
