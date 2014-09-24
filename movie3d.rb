3ca482e8f0c9312c67376da80077ef7ff50891e1
David G Chaves <davidgchaves@gmail.com>
2014-09-24 16:34:21 +0200

    20 - inheritance

---
+++ movie3d.rb
@@ -0,0 +1,16 @@
+require_relative 'movie'
+
+class Movie3D < Movie
+  def initialize(title, rank, wow_factor)
+    super title, rank
+    @wow_factor = wow_factor
+  end
+
+  def thumbs_up
+    @wow_factor.times { super }
+  end
+
+  def show_effect
+    puts "Wow! " * @wow_factor
+  end
+end
