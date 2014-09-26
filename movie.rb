361c41cc8b8a7b0a6ef03901d995c06c04a38b8d
David G Chaves <davidgchaves@gmail.com>
2014-09-26 14:38:18 +0200

    21 - mixins

--- movie.rb
+++ movie.rb
@@ -1,6 +1,9 @@
+require_relative 'rankable'
+
 class Movie
-  attr_accessor :title
-  attr_reader :rank
+  include Rankable
+
+  attr_accessor :title, :rank
 
   def initialize(title, rank=0)
     @title = title.capitalize
@@ -20,30 +23,6 @@ class Movie
     @snack_carbs[snack.name] += snack.carbs
     puts "#{@title} led to #{snack.carbs} #{snack.name} carbs being consumed."
     puts "#{@title}'s snack: #{@snack_carbs}"
-  end
-
-  def hit?
-    @rank >= 10
-  end
-
-  def status
-    hit? ? "Hit" : "Flop"
-  end
-
-  def normalized_rank
-    @rank / 10
-  end
-
-  def thumbs_up
-    @rank += 1
-  end
-
-  def thumbs_down
-    @rank -= 1
-  end
-
-  def <=>(other_movie)
-    other_movie.rank <=> @rank
   end
 
   def self.from_csv(line)
